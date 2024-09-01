20d9dfaab4e3877ef3b6e00f5b54a6a103183a9f
Patrick Stinson <patrickkidd@gmail.com>
2024-09-01 06:26:52 -0800

    Upload artifact windows

--- bin/pyqtdeploy-build-debuggable.py
+++ bin/pyqtdeploy-build-debuggable.py
@@ -1,41 +1,46 @@
-import os, os.path, sys
-from pyqtdeploy import pyqtdeploybuild_main
+from pyqtdeploy.pyqtdeploybuild_main import main
 
-path = os.path.abspath(os.path.join(__file__, "..", ".."))
-os.chdir(path)
+main()
 
 
-if os.name == "posix":
-    QMAKE = "/Users/patrick/dev/lib/Qt/5.15.2/clang_64/bin/qmake"
-    os.environ["PATH"] = (
-        "/Users/patrick/dev/familydiagram/.direnv/python-3.7.8/bin:"
-        + os.environ["PATH"]
-    )
-    sys.argv += [
-        "--verbose",
-        "--resources",
-        "12",
-        "--target",
-        "macos-64",
-        "--build-dir",
-        "build/osx",
-        "familydiagram.pdt",
-    ]
-else:
-    QMAKE = "C:\\Qt\\5.15.1\\msvc2019_64\\bin\\qmake.exe"
-    os.environ["PATH"] = (
-        "C:\\Strawberry\\perl\\bin;C:\\Users\\patrick\\AppData\\Local\\Programs\\Python\\Python37\\Scripts;"
-        + os.environ["PATH"]
-    )
-    sys.argv += [
-        "--verbose",
-        "--resources",
-        "12",
-        "--target",
-        "win-64",
-        "familydiagram.pdt",
-    ]
+# import os, os.path, sys
+# from pyqtdeploy import pyqtdeploybuild_main
 
-pyqtdeploybuild_main.main()
+# path = os.path.abspath(os.path.join(__file__, "..", ".."))
+# os.chdir(path)
 
-x = 1
+
+# if os.name == "posix":
+#     QMAKE = "/Users/patrick/dev/lib/Qt/5.15.2/clang_64/bin/qmake"
+#     os.environ["PATH"] = (
+#         "/Users/patrick/dev/familydiagram/.direnv/python-3.7.8/bin:"
+#         + os.environ["PATH"]
+#     )
+#     sys.argv += [
+#         "--verbose",
+#         "--resources",
+#         "12",
+#         "--target",
+#         "macos-64",
+#         "--build-dir",
+#         "build/osx",
+#         "familydiagram.pdt",
+#     ]
+# else:
+#     QMAKE = "C:\\Qt\\5.15.1\\msvc2019_64\\bin\\qmake.exe"
+#     os.environ["PATH"] = (
+#         "C:\\Strawberry\\perl\\bin;C:\\Users\\patrick\\AppData\\Local\\Programs\\Python\\Python37\\Scripts;"
+#         + os.environ["PATH"]
+#     )
+#     sys.argv += [
+#         "--verbose",
+#         "--resources",
+#         "12",
+#         "--target",
+#         "win-64",
+#         "familydiagram.pdt",
+#     ]
+
+# pyqtdeploybuild_main.main()
+
+# x = 1
