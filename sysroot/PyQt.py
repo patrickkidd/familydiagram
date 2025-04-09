750ba7df7c2659ec2d17101684a841e5e2114746
Patrick Stinson <patrickkidd@gmail.com>
2025-04-08 23:04:50 -0800

    sysroot builds now

--- sysroot/PyQt.py
+++ sysroot/PyQt.py
@@ -1,22 +1,36 @@
 import os
+import toml
 from pyqtdeploy.sysroot.plugins import PyQt
+from pyqtdeploy import ComponentOption
 
 
 class PyQtComponent(PyQt.PyQtComponent):
 
+    def get_options(self):
+        options = super().get_options()
+
+        options.append(
+            ComponentOption(
+                "extra_compile_args",
+                type=list,
+                help="Pass these on to each module in pyproject.toml.",
+            )
+        )
+
+        return options
+
     def run(self, *args, capture=False):
         """Add multiprocessing to make."""
-        qt = self.get_component("Qt")
 
         _args = tuple(args)
-        if args[0] == self.host_make:
-            self.verbose("Adding args for concurrent build.")
-            if os.name == "nt":
-                os.environ["CL"] = "/MP"
-            else:
-                _args += ("-j16",)
-        elif args[0] == qt.host_qmake and self.target_arch_name == "ios-64":
-            _args += ("QMAKE_CFLAGS=-Wno-deprecated-declarations",)
-        #     self.verbose('*********** Adding args for debug build.')
-        #     _args += ('CONFIG+=debug', 'QMAKE_CFLAGS=-g')
+        if args[0] == "sip-install":
+            # Add extra options to pyproject.toml before it's run.
+            pyproject = toml.load("pyproject.toml")
+            bindings_section = self._get_section("tool.sip.bindings", pyproject)
+            for module in bindings_section.values():
+                if isinstance(module, dict):
+                    module.update({"extra-compile-args": self.extra_compile_args})
+            with open("pyproject.toml", "w") as f:
+                toml.dump(pyproject, f)
+
         return super().run(*_args, capture=capture)
