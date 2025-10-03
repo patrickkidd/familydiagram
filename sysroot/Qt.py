83883f933785a1a135b055f28f0366de5436f366
Patrick Stinson <patrickkidd@gmail.com>
2025-10-03 13:15:38 -0800

    libpng patch

--- sysroot/Qt.py
+++ sysroot/Qt.py
@@ -28,6 +28,11 @@ class QtComponent(Qt.QtComponent):
                 "qtbase/src/plugins/platforms/cocoa/qiosurfacegraphicsbuffer.h",
                 self._patch_qiosurfacegraphicsbuffer,
             )
+            # Fix libpng fp.h issue on macOS 15+ SDK
+            self.patch_file(
+                "qtbase/src/3rdparty/libpng/pngpriv.h",
+                self._patch_libpng_pngpriv,
+            )
             # shutil.copyfile(
             #     "../../../toolchain-macos-13.prf",
             #     "qtbase/mkspecs/features/toolchain.prf",
@@ -71,3 +76,33 @@ class QtComponent(Qt.QtComponent):
                 "unix:!darwin:if(!cross_compile|host_build) {",
             )
         )
+
+    @staticmethod
+    def _patch_libpng_pngpriv(line, patch_file):
+        """Remove obsolete fp.h header for macOS 15+ SDK compatibility."""
+        # Skip the obsolete macOS compiler check block that includes fp.h
+        if "# if (defined(__MWERKS__)" in line:
+            # Skip lines until we find the closing #endif for this block
+            # Just write the standard math.h include instead
+            patch_file.write("# include <math.h>\n")
+            return
+
+        # Skip the continuation of the __MWERKS__ condition
+        if any(x in line for x in [
+            "defined(THINK_C)",
+            "/* We need to check that <math.h>",
+            "* as it seems it doesn't agree with <fp.h>",
+            "* <fp.h> if possible.",
+            "# if !defined(__MATH_H__)",
+            "# include <fp.h>",
+        ]):
+            return
+
+        # Skip the #else and second math.h include (we already added it)
+        if line.strip() == "# else" or (line.strip() == "# include <math.h>" and hasattr(patch_file, "_libpng_patched")):
+            if line.strip() == "# else":
+                patch_file._libpng_patched = True
+            return
+
+        # Write all other lines unchanged
+        patch_file.write(line)
