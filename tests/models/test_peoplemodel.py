ae677173ab349874b8ba60870f704f1619fc2fec
Patrick Stinson <patrickkidd@gmail.com>
2025-10-13 15:50:16 -0800

    test_peoplemodel.py

--- tests/models/test_peoplemodel.py
+++ tests/models/test_peoplemodel.py
@@ -6,10 +6,10 @@ from pkdiagram.models import PeopleModel
 
 
 @pytest.fixture
-def peopleModel(simpleScene):
-    pm = PeopleModel()
-    pm.scene = simpleScene
-    return pm
+def model(scene):
+    _model = PeopleModel()
+    _model.scene = scene
+    return _model
 
 
 def sortedPeople(scene):
@@ -29,61 +29,56 @@ def find(scene, name):
             return i
 
 
-def test_init(simpleScene, peopleModel):
-    people = sortedPeople(simpleScene)
+def test_init(scene, model):
+    p1, p2 = scene.addItems(Person(name="p1"), Person(name="p2"))
+    people = sortedPeople(scene)
     assert len(people) > 0
-    assert peopleModel.rowCount() == len(people)
+    assert model.rowCount() == len(people)
 
-    names = sortedNames(simpleScene)
-    for row in range(peopleModel.rowCount()):
-        index = peopleModel.index(row, 0)
-        assert peopleModel.data(index) == names[row]
+    names = sortedNames(scene)
+    for row in range(model.rowCount()):
+        index = model.index(row, 0)
+        assert model.data(index) == names[row]
 
 
-def test_add_person(simpleScene, peopleModel):
+def test_add_person(scene, model):
 
-    beforeRows = peopleModel.rowCount()
-    person = Person(name="Yo")
-    simpleScene.addItem(person)
-    assert peopleModel.rowCount() == beforeRows + 1
+    beforeRows = model.rowCount()
+    person = scene.addItem(Person(name="Yo"))
+    assert model.rowCount() == beforeRows + 1
 
     # shouldn't increase row count
-    person = Person()
-    simpleScene.addItem(person)
-    assert peopleModel.rowCount() == beforeRows + 1
+    person = scene.addItem(Person())
+    assert model.rowCount() == beforeRows + 1
 
 
-def test_remove_person(simpleScene, peopleModel):
+def test_remove_person(scene, model):
+    p1, p2 = scene.addItems(Person(name="p1"), Person(name="p2"))
 
-    beforeRows = peopleModel.rowCount()
+    beforeRows = model.rowCount()
     assert beforeRows > 0
 
-    simpleScene.removeItem(simpleScene.people()[0])
-    assert peopleModel.rowCount() == beforeRows - 1
+    scene.removeItem(scene.people()[0])
+    assert model.rowCount() == beforeRows - 1
 
 
-def test_change_person(simpleScene, peopleModel):
+def test_change_person(scene, model):
 
-    personA = Person(name="A")
-    personB = Person(name="B")
-    simpleScene.addItems(personA, personB)
+    personA, personB = scene.addItems(Person(name="A"), Person(name="B"))
 
-    aBeforeIndex = find(simpleScene, "A")
-    bBeforeIndex = find(simpleScene, "B")
+    aBeforeIndex = find(scene, "A")
+    bBeforeIndex = find(scene, "B")
     assert aBeforeIndex < bBeforeIndex
 
     personA.setName("C")
-    aAfterIndex = find(simpleScene, "C")
-    bAfterIndex = find(simpleScene, "B")
+    aAfterIndex = find(scene, "C")
+    bAfterIndex = find(scene, "B")
     assert aAfterIndex != aBeforeIndex
     assert bAfterIndex != bBeforeIndex
     assert aAfterIndex > bAfterIndex
 
 
-def test_set_existing_persons_name():
-    scene = Scene()
-    model = PeopleModel()
-    model.scene = scene
+def test_set_existing_persons_name(scene, model):
 
     rowsInserted = util.Condition(model.rowsInserted)
     personA = Person()
@@ -97,10 +92,7 @@ def test_set_existing_persons_name():
     assert find(scene, "A") == 0
 
 
-def test_set_existing_peoples_names():
-    scene = Scene()
-    model = PeopleModel()
-    model.scene = scene
+def test_set_existing_peoples_names(scene, model):
     personA = Person()
     personB = Person(name="B")
     scene.addItems(personA, personB)
@@ -121,14 +113,9 @@ def test_set_existing_peoples_names():
     assert model.index(1, 0).data(model.IdRole) == personB.id
 
 
-def test_clear_existing_persons_name():
-    scene = Scene()
-    model = PeopleModel()
-    rowsRemoved = util.Condition()
-    model.rowsRemoved.connect(rowsRemoved)
-    personA = Person(name="A")
-    scene.addItem(personA)
-    model.scene = scene
+def test_clear_existing_persons_name(scene, model):
+    rowsRemoved = util.Condition(model.rowsRemoved)
+    personA = scene.addItem(Person(name="A"))
     assert find(scene, "A") == 0
 
     personA.prop("name").reset()
@@ -138,14 +125,8 @@ def test_clear_existing_persons_name():
     assert find(scene, "A") == None
 
 
-def test_clear_existing_peoples_names():
-    scene = Scene()
-    model = PeopleModel()
-    model.scene = scene
-
-    personA = Person(name="A")
-    personB = Person(name="B")
-    scene.addItems(personA, personB)
+def test_clear_existing_peoples_names(scene, model):
+    personA, personB = scene.addItems(Person(name="A"), Person(name="B"))
     aAfterIndex = find(scene, "A")
     bAfterIndex = find(scene, "B")
     assert aAfterIndex == 0
@@ -158,26 +139,23 @@ def test_clear_existing_peoples_names():
     assert bAfterIndex == 0
 
 
-def test_accessors(peopleModel):
+def test_accessors(scene, model):
+    p1 = scene.addItem(Person(name="p1"))
 
-    assert peopleModel.idForRow(1000) == -1
-    assert peopleModel.rowForId(1000) == -1
+    assert model.idForRow(1000) == -1
+    assert model.rowForId(1000) == -1
 
-    assert peopleModel.idForRow(0) is not None
-    assert peopleModel.rowForId(peopleModel.idForRow(0)) == 0
+    assert model.idForRow(0) is not None
+    assert model.rowForId(model.idForRow(0)) == 0
 
 
-def test_sort():
-    scene = Scene()
-    model = PeopleModel()
-    model.scene = scene
+def test_sort(scene, model):
 
     def get(row):
         return model.data(model.index(row, 0), model.NameRole)
 
     # add just one
-    personA = Person(name="Aaa")
-    scene.addItem(personA)
+    personA = scene.addItem(Person(name="Aaa"))
     assert model.rowCount() == 1
     assert model.columnCount() == 1
     assert get(0) == "Aaa"
@@ -185,10 +163,8 @@ def test_sort():
     assert model.idForRow(1) == -1
 
     # add out of order
-    personC = Person(name="Ccc")
-    personEmpty = Person()  # shouldn't be listed
-    scene.addItem(personC)
-    scene.addItem(personEmpty)
+    personC = scene.addItem(Person(name="Ccc"))
+    personEmpty = scene.addItem(Person())  # shouldn't be listed
     assert model.rowCount() == 2
     assert get(0) == "Aaa"
     assert get(1) == "Ccc"
@@ -197,8 +173,7 @@ def test_sort():
     assert model.idForRow(2) == -1
 
     # add + sort to middle
-    personBD = Person(name="Bbb")
-    scene.addItem(personBD)
+    personBD = scene.addItem(Person(name="Bbb"))
     assert model.rowCount() == 3
     assert get(0) == "Aaa"
     assert get(1) == "Bbb"
@@ -227,17 +202,11 @@ def test_sort():
     assert model.idForRow(2) == -1
 
 
-def test_data():
+def test_data(scene, model):
 
-    scene = Scene()
-    personA = Person(name="Aaa")
-    personC = Person(name="Ccc")
-    personB = Person(name="Bbb")
-    scene.addItem(personA)
-    scene.addItem(personB)
-    scene.addItem(personC)
-    model = PeopleModel()
-    model.scene = scene
+    personA, personB, personC = scene.addItems(
+        Person(name="Aaa"), Person(name="Bbb"), Person(name="Ccc")
+    )
 
     def getName(row):
         return model.data(model.index(row, 0), model.NameRole)
