35207e8408f4a20662a82a3b6ca1ce4e6f000709
Dr. Patrick Stinson <patrickkidd@gmail.com>
2025-10-25 12:39:55 -0800

    FD-260: Remove pipenv from CD (#63)
    * remove pipenv
    
    * --extra-index-url=$TWINE_REPOSITORY_URL
    
    * activate
    
    * GITHUB_ACTIONS

--- sysroot/btcopilot.py
+++ sysroot/btcopilot.py
@@ -1,4 +1,4 @@
-import os.path
+import os, os.path
 from pyqtdeploy import Component, PythonPackage
 
 
@@ -15,12 +15,13 @@ class BTCopilotComponent(Component):
         import sysconfig
 
         # The site-packages dir, for wheels
-        # return sysconfig.get_paths()["purelib"]
-
-        # expect it to be cloned alongside this repo for any build?
-        return os.path.abspath(
-            os.path.join(os.path.dirname(__file__), "..", "..", "btcopilot")
-        )
+        if os.getenv("GITHUB_ACTIONS"):
+            return sysconfig.get_paths()["purelib"]
+        else:
+            # dev; could even just import and detect the path from there.
+            return os.path.abspath(
+                os.path.join(os.path.dirname(__file__), "..", "..", "btcopilot")
+            )
 
     def install(self):
         pass
