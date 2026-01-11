1851f1213e56c117ffa2a900e8ae832d8a65575d
Patrick Stinson <patrickkidd@gmail.com>
2026-01-11 06:13:14 -0900

    setObjectOwnership

--- pkdiagram/models/scenemodel.py
+++ pkdiagram/models/scenemodel.py
@@ -136,7 +136,8 @@ class SceneModel(QObject, ModelHelper):
     @pyqtSlot(int, result=QObject)
     def item(self, id):
         ret = self.scene.findById(id)
-        QQmlEngine.setObjectOwnership(ret, QQmlEngine.CppOwnership)
+        if isinstance(ret, QObject):
+            QQmlEngine.setObjectOwnership(ret, QQmlEngine.CppOwnership)
         return ret
 
 
