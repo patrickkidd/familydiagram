7021fb5bd59680db8b53e298f9b5b89f5363efdc
Patrick Stinson <patrickkidd@gmail.com>
2026-02-13 11:18:34 -0900

    Revert "Added chat voice dictation"
    This reverts commit 56a219f3317a277d21f38d97bd99b819ae788aa2.

--- _pkdiagram/_pkdiagram_mac.mm
+++ _pkdiagram/_pkdiagram_mac.mm
@@ -3,7 +3,6 @@
 #include <TargetConditionals.h>
 #if TARGET_OS_IOS
 #import <UIKit/UIKit.h>
-#import <AVFoundation/AVFoundation.h>
 #else
 #import <AppKit/AppKit.h>
 #endif
@@ -36,13 +35,6 @@ static NSURL *SparkleFeedURL = nil;
 // iOS requires CALayer modifications on main thread; force single-threaded render loop
 __attribute__((constructor)) static void setRenderLoopForIOS() {
     setenv("QSG_RENDER_LOOP", "basic", 0);
-}
-
-// AVSpeechSynthesizer requires an active audio session with playback category
-__attribute__((constructor)) static void setupAudioSessionForIOS() {
-    AVAudioSession *session = [AVAudioSession sharedInstance];
-    [session setCategory:AVAudioSessionCategoryPlayback error:nil];
-    [session setActive:YES error:nil];
 }
 #endif
 
@@ -2018,15 +2010,6 @@ bool CUtil::isiOSSimulator() {
 #endif
 }
 */
-
-void CUtil::openNativeUrl(const QString &url) {
-    NSURL *nsUrl = [NSURL URLWithString:url.toNSString()];
-#if TARGET_OS_IOS
-    [[UIApplication sharedApplication] openURL:nsUrl options:@{} completionHandler:nil];
-#else
-    [[NSWorkspace sharedWorkspace] openURL:nsUrl];
-#endif
-}
 
 CUtil::OperatingSystem CUtil::operatingSystem() {
     int ret = 0;
